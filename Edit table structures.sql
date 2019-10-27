b2f29ef04e7aabb48d5f77ac5c48bd4f1f6ffaaf
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2019-10-27 21:07:18 +0200

    Description
    Adding description column for Products table

--- Edit table structures.sql
+++ Edit table structures.sql
@@ -1,10 +1,16 @@
 USE ShopDB;
 GO
 
+--Adding description column for Products table
+ALTER TABLE Products.Category
+ADD Description varchar(100);
+GO
+
 --Mistakenly forgot to add Salary column when creating employee table
 ALTER TABLE Staff.Employees
 ADD Salary decimal(18, 2) NOT NULL;
 
+--Just view currently available columns
 SELECT * FROM Products.Manufacturer
 
 --Column forgotten when creating Manufacturer table
