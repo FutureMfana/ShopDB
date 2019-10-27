e9a2353809d018cd61d7d4235b252cbc5f6d404a
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2019-10-27 21:00:36 +0200

    Edit table structure
    This script is responsible for editig table structures where there is a need

---
+++ Edit table structures.sql
@@ -0,0 +1,25 @@
+USE ShopDB;
+GO
+
+--Mistakenly forgot to add Salary column when creating employee table
+ALTER TABLE Staff.Employees
+ADD Salary decimal(18, 2) NOT NULL;
+
+SELECT * FROM Products.Manufacturer
+
+--Column forgotten when creating Manufacturer table
+ALTER TABLE Products.Manufacturer
+ADD City varchar(30) NOT NULL;
+
+ALTER TABLE Products.Manufacturer
+ADD Country varchar(30) NOT NULL;
+
+ALTER TABLE Products.Manufacturer
+ADD PostalCode varchar(10) NOT NULL;
+
+ALTER TABLE Products.Manufacturer
+ADD Tel char(10) NOT NULL;
+
+--Changing datatype to accommodate national charaters (UNICODE)
+ALTER TABLE Products.Manufacturer
+ALTER COLUMN Manufacturer nvarchar(50) NOT NULL;
